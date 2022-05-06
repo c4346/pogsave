7fb184fbadd2c15a7c6c8da6bda9b167743bda04
c4346 <53945103+c4346@users.noreply.github.com>
2022-05-05 22:15:27 -0400

    Update Pogsave v2.0.cpp

--- Pogsave v2.0.cpp
+++ Pogsave v2.0.cpp
@@ -1,30 +1,29 @@
-// Pogsave v2.0.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <fstream>
 #include <filesystem>
 #include <string>
-#include "Windows.h"
 namespace fs = std::filesystem;
 
 int main()
 {
-    std::ofstream Pog("Pogsave.txt");
-    Pog << "pogsave";
-    Pog.close();
-    int i = 0;
-    fs::path p1 = fs::current_path();
-    std::string path_to_destroy = p1.string();
-    path_to_destroy += "x64";
-
+    std::string format = ".txt";
+    std::string path_to_destroy = "C://Code//c++//Pogsave v2.0//Pogsave v2.0//Test";
 
     
-    for (const fs::directory_entry& dir_entry : fs::recursive_directory_iterator(p1)) {
-        std::cout << dir_entry << "\n";
-        i++;
+    for (const fs::directory_entry& dir_entry : fs::recursive_directory_iterator(path_to_destroy)) {
+        std::cout << dir_entry;
+        if (dir_entry.is_regular_file()) {
+            std::cout << "  is a file\n\n";
+        }
+        else {
+            std::cout << " is a directory  \n";
+            std::ofstream Pog(dir_entry.path().string() + "\\Pogsave" + format);
+            Pog << "hi";
+            Pog.close();
+            std::cout << "placed file at : " << dir_entry << "\n\n";
+        }
     }
-
+    
     
 
 }
